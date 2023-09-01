ed9b4fa15f518bf9ac8fc1d54ece279bd2e5fc9e
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2023-09-01 07:51:16 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Aplicacion_Recursividad_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn1
+            // 
+            this.Btn1.Location = new System.Drawing.Point(92, 284);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(330, 104);
+            this.Btn1.TabIndex = 0;
+            this.Btn1.Text = "Mostrar";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(487, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(749, 654);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 225);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(380, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ruta:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1393, 720);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicacion_Recursividad_WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Obtener la ruta ingresada por el usuario desde el TextBox.
+            string directorio = textBox1.Text;
+
+            if (Directory.Exists(directorio))
+            {
+                // Limpiar el ListBox antes de listar los archivos.
+                listBox1.Items.Clear();
+
+                // Llamar a la función recursiva para listar los archivos.
+                ListarArchivosRecursivamente(directorio);
+            }
+            else
+            {
+                MessageBox.Show("El directorio no existe.", "Directorio no encontrado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+        private void ListarArchivosRecursivamente(string directorio)
+        {
+            string[] archivos = Directory.GetFiles(directorio);
+
+            foreach (string archivo in archivos)
+            {
+                string nombreArchivo = Path.GetFileName(archivo);
+                string extension = Path.GetExtension(archivo);
+                listBox1.Items.Add($"Archivo: {nombreArchivo}, Extensión: {extension}");
+            }
+
+            string[] subdirectorios = Directory.GetDirectories(directorio);
+
+            foreach (string subdirectorio in subdirectorios)
+            {
+                // Llamar recursivamente a la función para explorar los subdirectorios.
+                ListarArchivosRecursivamente(subdirectorio);
+            }
+        }
+    }
+}
